88b186592cfd9b5fa543be9005160e375033d8f5
Adrian Olszewski <adrian.olszewski88@gmail.com>
2019-02-12 09:16:50 +0100

    deleted arrows style

---
+++ JS/portfolio.js
@@ -0,0 +1,14 @@
+// $(".arrowright").on("click", function () {
+//     $(".right").addClass("active");
+// })
+
+
+document.querySelector('.arrowright').addEventListener('click', function () {
+    document.querySelector('.right').classList.add("active");
+    document.querySelector('.center').classList.add("off")
+})
+
+document.querySelector('.back').addEventListener('click', function () {
+    document.querySelector('.right').classList.remove("active");
+    document.querySelector('.center').classList.remove("off")
+})
