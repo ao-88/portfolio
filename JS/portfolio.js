3cab13787ab0155c2c946468b3bfc97c6004f172
Adrian Olszewski <adrian.olszewski88@gmail.com>
2019-02-12 13:53:06 +0100

    new nav

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -3,12 +3,19 @@
 // })
 
 
-document.querySelector('.arrowright').addEventListener('click', function () {
-    document.querySelector('.right').classList.add("active");
-    document.querySelector('.center').classList.add("off")
-})
+// document.querySelector('.arrowright').addEventListener('click', function () {
+//     document.querySelector('.right').classList.add("active");
+//     document.querySelector('.center').classList.add("off")
+// })
 
-document.querySelector('.back').addEventListener('click', function () {
-    document.querySelector('.right').classList.remove("active");
-    document.querySelector('.center').classList.remove("off")
+// document.querySelector('.back').addEventListener('click', function () {
+//     document.querySelector('.right').classList.remove("active");
+//     document.querySelector('.center').classList.remove("off")
+// })
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', function () {
+    menu.classList.toggle("active");
 })
